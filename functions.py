334c578197db5068ff91287e081715fccdcb3dd2
romazepa <r.prilepskiy@gmail.com>
2014-12-26 03:03:57 +0300

    v2.0 - calcs the stats for all friends of the user
    Testing for the whole list of friends

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ __author__ = 'Roman'
 # -*- coding: UTF-8 -*-
 import urllib
 import json
+import timeit
 
 from python_project.settings import *
 
@@ -15,8 +16,9 @@ def get_user(uid = default_id):
 def get_user_friends(uid = default_id):
     raw_data = urllib.urlopen(API + 'friends.get?user_id=%s&v=5.27' % uid)
     data = json.load(raw_data)
-    print str(data['response']['count']) + ' friends'
-    return data['response']['items']
+    # print str(data['response']['count']) + ' friends'
+    # return data['response']['items']
+    return data['response']['count'], data['response']['items']
 
 def get_common_friends(source_uid, target_uid):
     raw_data = urllib.urlopen(API + 'friends.getMutual?source_uid=%s'
@@ -24,14 +26,16 @@ def get_common_friends(source_uid, target_uid):
                                     '&v=5.27'
                                     '&access_token=%s'% (source_uid, target_uid, token))
     data = json.load(raw_data)
-    print str(len(data['response'])) + ' mutual friends with %s'% (target_uid)
-    return data["response"]
+    # print str(len(data['response'])) + ' mutual friends with %s'% (target_uid)
+    # return data["response"]
+    return len(data['response'])
 
 def get_user_followers(uid = default_id):
     raw_data = urllib.urlopen(API + 'users.getFollowers?user_id=%s&v=5.27' % uid)
     data = json.load(raw_data)
-    print str(data['response']['count']) + ' followers'
-    return data['response']['items']
+    # print str(data['response']['count']) + ' followers'
+    # return data['response']['items']
+    return data['response']['count']
 
 def get_wall_posts(uid = default_id, offset = 0, count = 100):
     # 100 posts max at a time
@@ -67,8 +71,11 @@ def get_max_post(uid, posts_data, total_posts, flag, top_num):
         new_post_data = get_wall_posts(uid, offset)
         temp.extend( get_max_from_100(new_post_data, flag) )
         temp = sort_list_of_tuples(temp)[0:top_num]
-        print str(offset + 100) + ' posts analyzed...'
+        # print str(offset + 100) + ' posts analyzed...'
         offset += 100
+        print '.',
+
+    print ' ' + flag + ' is analyzed.'
 
     return sort_list_of_tuples(temp)[0:top_num]
 
@@ -100,36 +107,64 @@ def print_top_posts(top_posts_data, flag, top_num, uid = default_id):
     for i in range(0, top_num):
         print_one_post_data(top_posts_data[i], i+1, top_num, flag, uid)
 
-def print_line_to_file(uid, num_friends, num_common_friends, num_followers, num_posts, num_max_likes, num_max_comments, num_max_reposts):
-    with open('data.csv' 'w+') as fp:
-        fp.write(uid, num_friends, num_common_friends, num_followers, num_posts, num_max_likes, num_max_comments, num_max_reposts)
+def print_to_file(uid, num_friends, num_common_friends, num_followers, num_posts, num_max_likes, num_max_comments, num_max_reposts):
+    with open('data.csv', 'a+') as fp:
+        fp.write( str(uid) + ';'
+                  + str(num_friends) + ';'
+                  + str(num_common_friends) + ';'
+                  + str(num_followers) + ';'
+                  + str(num_posts) + ';'
+                  + str(num_max_likes) + ';'
+                  + str(num_max_comments) + ';'
+                  + str(num_max_reposts) + '\n'
+        )
         fp.close()
 
-target_uid = '5887277'
-default_id = '1844128'
-get_user(default_id)
-get_user_friends(default_id)
-get_common_friends(str(default_id), target_uid)
-get_user_followers(default_id)
-total_posts, posts = get_wall_posts(default_id)
-print str(total_posts) + ' posts'
+def compute_top_posts(flag, posts, total_posts, uid = default_id):
+    top_num = 5
+    top_posts = get_max_post(uid, posts, total_posts, flag, top_num)
+    post_id, flag_count = top_posts[0]
 
-# flag = 'likes'
-# get_max_post(default_id, posts, total_posts, flag)
-flag = 'comments'
-top_num = 5
-top_posts = get_max_post(default_id, posts, total_posts, flag, top_num)
-print top_posts
-print_top_posts(top_posts, flag, top_num, default_id)
+    # print_top_posts(top_posts, flag, top_num, uid)
+    return flag_count
 
-# print_line_to_file(int(default_id), num_friends, num_common_friends, num_followers, num_posts, num_max_likes, num_max_comments, num_max_reposts)
-# flag = 'reposts'
-# get_max_post(default_id, posts, total_posts, flag)
+def compute_all_for_uid(uid, target_uid, counter):
+    print str(counter) + '.',
+    get_user(uid)
+    num_friends, friends = get_user_friends(uid)
+    num_common_friends = get_common_friends(uid, target_uid)
+    num_followers = get_user_followers(uid)
+    total_posts, posts = get_wall_posts(uid)
 
-# posts = get_wall_posts(default_id, 100)
+    # print str(total_posts) + ' posts'
 
-#print 'TOP post: http://vk.com/id%s?w=wall%s_%s' % ( user_id'], top_users[i]['user_id'], top_users[i]['top_posts'][0]['id'] )
-# posts[i]['id']
-# posts[i]['comments']['count']
-# posts[i]['likes']['count']
-# posts[i]['reposts']['count']
+    flag1 = 'likes'
+    flag2 = 'comments'
+    flag3 = 'reposts'
+
+    num_max_likes = compute_top_posts(flag1, posts, total_posts, uid)
+    num_max_comments = compute_top_posts(flag2, posts, total_posts, uid)
+    num_max_reposts = compute_top_posts(flag3, posts, total_posts, uid)
+
+    print_to_file(uid, num_friends, num_common_friends, num_followers, total_posts, num_max_likes, num_max_comments, num_max_reposts)
+
+def main():
+    target_uid = '801017'
+    # default_id = '1164130' #Vadim Shishev
+    # default_id = '15314317' #Ilya Kurochkin
+    # default_id = '1844128' #Sanya Tyshkovskiy
+
+    num_friends, friends = get_user_friends(target_uid)
+    # compute_all_for_uid(target_uid)
+    counter = 1
+    for i in friends:
+        # get_user(i)
+        compute_all_for_uid(i, target_uid, counter)
+        counter += 1
+        # if counter == 3:
+        #     break
+
+start = timeit.timeit()
+main()
+stop = timeit.timeit()
+print stop - start
